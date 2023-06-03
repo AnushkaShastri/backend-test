256ba2bd316c65c826b569977ca3eb436e4b7b4e
anushka.shastri <anushka.shastri@impetus.com>
2023-06-03 15:11:23 +0530

    ec2-volume-testing-2

---
+++ infra/main.tf
@@ -0,0 +1,249 @@
+provider "aws" {
+  region = var.region
+  default_tags {
+    tags = {
+      createdBy        = var.createdBy
+      project          = var.project
+      projectComponent = var.projectComponent
+    }
+  }
+}
+
+terraform {
+  backend "http" {
+    update_method = "POST"
+    lock_method   = "POST"
+    unlock_method = "POST"
+  }
+}
+
+locals {
+  instance_count              = (var.instance_count == "null" || var.instance_count == null) ? 0 : var.instance_count
+  associate_public_ip_address = var.associate_public_ip_address == "null" ? null : var.associate_public_ip_address
+  availability_zone           = var.availability_zone == "null" ? null : var.availability_zone
+  is_t_instance_type                   = replace(var.instance_type, "/^t(2|3|3a){1}\\..*$/", "1") == "1" ? true : false
+  disable_api_stop                     = var.disable_api_stop == "null" ? null : var.disable_api_stop
+  disable_api_termination              = var.disable_api_termination == "null" ? null : var.disable_api_termination
+  ebs_optimized                        = var.ebs_optimized == "null" ? null : var.ebs_optimized
+  get_password_data                    = var.get_password_data == "null" ? null : var.get_password_data
+  hibernation                          = var.hibernation == "null" ? null : var.hibernation
+  host_id                              = var.host_id == "null" ? null : var.host_id
+  host_resource_group_arn              = var.host_resource_group_arn == "null" ? null : var.host_resource_group_arn
+  iam_instance_profile                 = var.iam_instance_profile == "null" ? null : var.iam_instance_profile
+  instance_initiated_shutdown_behavior = var.instance_initiated_shutdown_behavior == "null" ? null : var.instance_initiated_shutdown_behavior
+  instance_type                        = var.instance_type == "null" ? null : var.instance_type
+  ipv6_address_count                   = var.ipv6_address_count == "null" ? null : var.ipv6_address_count
+  key_name                             = var.key_name == "null" ? null : var.key_name
+  monitoring                           = var.monitoring == "null" ? null : var.monitoring
+  placement_group                      = var.placement_group == "null" ? null : var.placement_group
+  placement_partition_number           = var.placement_partition_number == "null" ? null : var.placement_partition_number
+  private_ip                           = var.private_ip == "null" ? null : var.private_ip
+  source_dest_check                    = var.source_dest_check == "null" ? null : var.source_dest_check
+  subnet_id                            = var.subnet_id == "null" ? null : var.subnet_id
+  tenancy                              = var.tenancy == "null" ? null : var.tenancy
+  user_data                            = var.user_data == "null" ? null : var.user_data
+  user_data_base64                     = var.user_data_base64 == "null" ? null : var.user_data_base64
+  user_data_replace_on_change          = var.user_data_replace_on_change == "null" ? null : var.user_data_replace_on_change
+  volume_attachment_count              = (var.volume_attachment_count == "null" || var.volume_attachment_count == null) ? 0 : var.volume_attachment_count
+  aws_volume_attachment_device         = var.aws_volume_attachment_device == "null" ? null : var.aws_volume_attachment_device
+  volume_force_detach                  = var.volume_force_detach == "null" ? null : var.volume_force_detach
+  skip_destroy                         = var.skip_destroy == "null" ? null : var.skip_destroy
+  stop_instance_before_detaching       = var.stop_instance_before_detaching == "null" ? null : var.stop_instance_before_detaching
+  ebs_encrypted                        = var.ebs_encrypted == "null" ? null : var.ebs_encrypted
+  final_snapshot                       = var.final_snapshot == "null" ? null : var.final_snapshot
+  multi_attach_enabled                 = var.multi_attach_enabled == "null" ? null : var.multi_attach_enabled
+  ebs_iops                             = var.ebs_iops == "null" ? null : var.ebs_iops
+  ebs_size                             = var.ebs_size == "null" ? null : var.ebs_size
+  snapshot_id                          = var.snapshot_id == "null" ? null : var.snapshot_id
+  outpost_arn                          = var.outpost_arn == "null" ? null : var.outpost_arn
+  ebs_type                             = var.ebs_type == "null" ? null : var.ebs_type
+  kms_key_id                           = var.kms_key_id == "null" ? null : var.kms_key_id
+  throughput                           = var.throughput == "null" ? null : var.throughput
+}
+
+module "security_group" {
+  source              = "terraform-aws-modules/security-group/aws"
+  name                = "${var.env}_${var.security_group_name}"
+  description         = var.sg_description
+  vpc_id              = var.vpc_id
+  ingress_cidr_blocks = var.ingress_cidr_blocks
+  ingress_rules       = var.ingress_rules
+  egress_rules        = var.egress_rules
+}
+
+resource "aws_instance" "instance" {
+  count                       = local.instance_count
+  ami                         = var.ami_id
+  associate_public_ip_address = local.associate_public_ip_address
+  availability_zone           = local.availability_zone
+  dynamic "capacity_reservation_specification" {
+    for_each = length(var.capacity_reservation_specification) == 0 ? [] : [var.capacity_reservation_specification]
+    content {
+      capacity_reservation_preference = lookup(capacity_reservation_specification.value, "capacity_reservation_preference", null)
+      capacity_reservation_target {
+          capacity_reservation_id                 = lookup(capacity_reservation_specification.value, "capacity_reservation_id", null)
+          capacity_reservation_resource_group_arn = lookup(capacity_reservation_specification.value, "capacity_reservation_resource_group_arn", null)
+      }
+    }
+  }
+  dynamic "cpu_options" {
+    for_each = length(var.cpu_options) == 0 ? [] : [var.cpu_options]
+    content {
+      amd_sev_snp      = lookup(cpu_options.value, "amd_sev_snp", null)
+      core_count       = lookup(cpu_options.value, "core_count", null)
+      threads_per_core = lookup(cpu_options.value, "threads_per_core", null)
+    }
+  }
+  dynamic "credit_specification" {
+    for_each = length(var.credit_specification) == 0 ? [] : [var.credit_specification]
+    content {
+      cpu_credits = local.is_t_instance_type == true ? lookup(credit_specification.value, "cpu_credits", null) : null
+    }
+  }
+  disable_api_stop        = local.disable_api_stop
+  disable_api_termination = local.disable_api_termination
+  dynamic "ebs_block_device" {
+    for_each = var.ebs_block_device
+    content {
+      delete_on_termination = lookup(ebs_block_device.value, "ebs_delete_on_termination", null)
+      device_name           = ebs_block_device.value.ebs_device_name
+      encrypted             = lookup(ebs_block_device.value, "ebs_encrypted", null)
+      iops                  = lookup(ebs_block_device.value, "ebs_iops", null)
+      kms_key_id            = lookup(ebs_block_device.value, "ebs_kms_key_id", null)
+      snapshot_id           = lookup(ebs_block_device.value, "ebs_snapshot_id", null)
+      tags                  = lookup(ebs_block_device.value, "ebs_tags", null)
+      throughput            = lookup(ebs_block_device.value, "ebs_throughput", null)
+      volume_size           = lookup(ebs_block_device.value, "ebs_volume_size", null)
+      volume_type           = lookup(ebs_block_device.value, "ebs_volume_type", null)
+    }
+  }
+  ebs_optimized = local.ebs_optimized
+  dynamic "enclave_options" {
+    for_each = length(var.enclave_options) == 0 ? [] : [var.enclave_options]
+    content {
+      enabled = lookup(enclave_options.value, "enclave_enabled", null)
+    }
+  }
+  dynamic "ephemeral_block_device" {
+    for_each = var.ephemeral_block_device
+    content {
+      device_name  = ephemeral_block_device.value.device_name
+      no_device    = lookup(ephemeral_block_device.value, "ephemeral_no_device", null)
+      virtual_name = lookup(ephemeral_block_device.value, "ephemeral_virtual_name", null)
+    }
+  }
+  get_password_data                    = local.get_password_data
+  hibernation                          = local.hibernation
+  host_id                              = local.host_id
+  host_resource_group_arn              = local.host_resource_group_arn
+  iam_instance_profile                 = local.iam_instance_profile
+  instance_initiated_shutdown_behavior = local.instance_initiated_shutdown_behavior
+  instance_type                        = local.instance_type
+  ipv6_address_count                   = local.ipv6_address_count
+  ipv6_addresses                       = var.ipv6_addresses
+  key_name                             = local.key_name
+  dynamic "launch_template" {
+    for_each = length(var.launch_template) == 0 ? [] : [var.launch_template]
+    content {
+      id      = lookup(launch_template.value, "launch_template_id", null)
+      name    = lookup(launch_template.value, "launch_template_name", null)
+      version = lookup(launch_template.value, "launch_template_version", null)
+    }
+  }
+  dynamic "maintenance_options" {
+    for_each = length(var.maintenance_options) == 0 ? [] : [var.maintenance_options]
+    content {
+      auto_recovery = lookup(maintenance_options.value, "auto_recovery", null)
+    }
+  }
+  dynamic "metadata_options" {
+    for_each = length(var.metadata_options) == 0 ? [] : [var.metadata_options]
+    content {
+      http_endpoint               = lookup(metadata_options.value, "metadata_http_endpoint", null)
+      http_put_response_hop_limit = lookup(metadata_options.value, "metadata_http_put_response_hop_limit", null)
+      http_tokens                 = lookup(metadata_options.value, "metadata_http_tokens", null)
+      instance_metadata_tags      = lookup(metadata_options.value, "metadata_instance_metadata_tags", null)
+    }
+  }
+  monitoring = local.monitoring
+  dynamic "network_interface" {
+    for_each = var.network_interface
+    content {
+      delete_on_termination = lookup(network_interface.value, "delete_on_termination", false)
+      device_index          = network_interface.value.device_index
+      network_card_index    = lookup(network_interface.value, "network_card_index", null)
+      network_interface_id  = network_interface.value.network_interface_id
+    }
+  }
+  placement_group            = local.placement_group
+  placement_partition_number = local.placement_partition_number
+  dynamic "private_dns_name_options" {
+    for_each = length(var.private_dns_name_options) == 0 ? [] : [var.private_dns_name_options]
+    content {
+      enable_resource_name_dns_aaaa_record = lookup(private_dns_name_options.value, "enable_resource_name_dns_aaaa_record", null)
+      enable_resource_name_dns_a_record    = lookup(private_dns_name_options.value, "enable_resource_name_dns_a_record", null)
+      hostname_type                        = lookup(private_dns_name_options.value, "hostname_type", null)
+    }
+  }
+  private_ip = length(var.private_ips) > 0 ? element(var.private_ips, count.index) : local.private_ip
+  dynamic "root_block_device" {
+    for_each = var.root_block_device
+    content {
+      delete_on_termination = lookup(root_block_device.value, "delete_on_termination", null)
+      encrypted             = lookup(root_block_device.value, "encrypted", null)
+      iops                  = lookup(root_block_device.value, "iops", null)
+      kms_key_id            = lookup(root_block_device.value, "kms_key_id", null)
+      tags                  = lookup(root_block_device.value, "tags", null)
+      throughput            = lookup(root_block_device.value, "throughput", null)
+      volume_size           = lookup(root_block_device.value, "volume_size", null)
+      volume_type           = lookup(root_block_device.value, "volume_type", null)
+    }
+  }
+  secondary_private_ips = var.secondary_private_ips
+  security_groups       = var.security_groups
+  source_dest_check     = length(var.network_interface) > 0 ? null : local.source_dest_check
+  subnet_id             = local.subnet_id
+  tags = merge(
+    {
+      "Name" = local.instance_count > 1 || var.use_num_suffix ? format("%s-%d", "${var.env}_${var.name}", count.index + 1) : "${var.env}_${var.name}"
+    },
+    var.tags,
+  )
+  tenancy                     = local.tenancy
+  user_data                   = local.user_data
+  user_data_base64            = local.user_data_base64
+  user_data_replace_on_change = local.user_data_replace_on_change
+  volume_tags = merge(
+    {
+      "Name" = local.instance_count > 1 || var.use_num_suffix ? format("%s-%d", "${var.env}_${var.name}", count.index + 1) : "${var.env}_${var.name}"
+    },
+    var.volume_tags,
+  )
+  vpc_security_group_ids = [module.security_group.security_group_id]
+}
+
+resource "aws_volume_attachment" "this_ec2" {
+  count                          = local.volume_attachment_count
+  device_name                    = local.aws_volume_attachment_device
+  instance_id                    = aws_instance.instance[count.index].id
+  volume_id                      = aws_ebs_volume.this[count.index].id
+  force_detach                   = local.volume_force_detach
+  skip_destroy                   = local.skip_destroy
+  stop_instance_before_detaching = local.stop_instance_before_detaching
+}
+
+resource "aws_ebs_volume" "this" {
+  count                = local.volume_attachment_count
+  availability_zone    = aws_instance.instance[count.index].availability_zone
+  encrypted            = local.ebs_encrypted
+  final_snapshot       = local.final_snapshot
+  iops                 = local.ebs_iops
+  multi_attach_enabled = local.multi_attach_enabled
+  size                 = local.ebs_size
+  snapshot_id          = local.snapshot_id
+  outpost_arn          = local.outpost_arn
+  type                 = local.ebs_type
+  kms_key_id           = local.kms_key_id
+  tags                 = var.ebs_tags
+  throughput           = local.throughput
+}
---
+++ infra/variables.tf
@@ -0,0 +1,437 @@
+variable "env" {
+  description = "Environment for which all the resources are being provisioned, being used as prefix for resource name"
+  type        = string
+  default     = "dev"
+}
+
+variable "createdBy" {
+  description = "Tag for each resource, reflects author name"
+  type        = string
+  default     = "KloudJet"
+}
+
+variable "project" {
+  description = "Tag for each resource, reflects Project ID (Find your Project ID on the Kloudjet Portal, passing incorrect or null value will disturb the metrics)"
+  type        = string
+  default     = "PID123"
+}
+
+variable "projectComponent" {
+  description = "Tag for each resource, reflects Component ID (Find your Project Component ID on the Kloudjet Portal, passing incorrect or null value will disturb the metrics)"
+  type        = string
+  default     = "CID123"
+}
+
+variable "region" {
+  description = "AWS region where all resources will be deployed"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "ami_id" {
+  description = "AMI to use for the instance"
+  type        = string
+  default     = "ami-0715c1897453cabd1"
+}
+
+variable "security_group_name" {
+  description = "Name of security group"
+  type        = string
+  default     = "kloudjet_security_group_ec2"
+}
+
+variable "sg_description" {
+  description = "Description of the EC2 Security Group"
+  type        = string
+  default     = "Security group for usage with EC2 instance"
+}
+
+variable "vpc_id" {
+  description = "The id of the specific VPC to retrieve"
+  type        = string
+  default     = null
+}
+
+variable "ingress_cidr_blocks" {
+  description = "List of CIDR blocks"
+  type        = list(string)
+  default     = ["0.0.0.0/0"]
+}
+
+variable "ingress_rules" {
+  description = "List of ingress rules to create by name"
+  type        = list(string)
+  default     = ["http-80-tcp", "all-icmp", "ssh-tcp"]
+}
+
+variable "egress_rules" {
+  description = "List of egress rules to create by name"
+  type        = list(string)
+  default     = ["all-all"]
+}
+
+variable "instance_count" {
+  description = "Number of EC2 instances to launch"
+  type        = number
+  default     = 1
+}
+
+variable "associate_public_ip_address" {
+  description = "If true, the EC2 instance will have associated public IP address"
+  type        = bool
+  default     = true
+}
+
+variable "availability_zone" {
+  description = "AZ to start the instance in"
+  type        = string
+  default     = null
+}
+
+variable "capacity_reservation_specification" {
+  description = "Describes an instance's Capacity Reservation targeting option"
+  type        = map(any)
+  default     = {}
+}
+
+variable "cpu_options" {
+  description = "The CPU options for the instance"
+  type        = map(any)
+  default     = {}
+}
+
+variable "credit_specification" {
+  description = "Configuration block for customizing the credit specification of the instance"
+  type        = map(any)
+  default     = {}
+}
+
+variable "disable_api_stop" {
+  description = "If true, enables EC2 Instance Stop Protection"
+  type        = bool
+  default     = null
+}
+
+variable "disable_api_termination" {
+  description = "If true, enables EC2 Instance Termination Protection"
+  type        = bool
+  default     = null
+}
+
+variable "ebs_block_device" {
+  description = "Additional EBS block devices to attach to the instance"
+  type        = list(any)
+  default     = []
+}
+
+variable "ebs_optimized" {
+  description = "If true, the launched EC2 instance will be EBS-optimized, if the instance type is optimized by default then there is no need to set this and there is no effect to disabling it"
+  type        = bool
+  default     = false
+}
+
+variable "enclave_options" {
+  description = "Whether Nitro Enclaves will be enabled on the instance, defaults to false"
+  type        = map(any)
+  default     = {}
+}
+
+variable "ephemeral_block_device" {
+  description = "Customize Ephemeral (also known as Instance Store) volumes on the instance"
+  type        = list(any)
+  default     = []
+}
+
+variable "get_password_data" {
+  description = "If true, wait for password data to become available and retrieve it."
+  type        = bool
+  default     = false
+}
+
+variable "hibernation" {
+  description = "If true, the launched EC2 instance will support hibernation"
+  type        = bool
+  default     = null
+}
+
+variable "host_id" {
+  description = "ID of a dedicated host that the instance will be assigned to, use when an instance is to be launched on a specific dedicated host"
+  type        = string
+  default     = null
+}
+
+variable "host_resource_group_arn" {
+  description = "ARN of the host resource group in which to launch the instances, if you specify an ARN, omit the tenancy parameter or set it to host"
+  type        = string
+  default     = null
+}
+
+variable "iam_instance_profile" {
+  description = "The IAM Instance Profile to launch the instance with, specified as the name of the Instance Profile"
+  type        = string
+  default     = "EC2_Can_Use_Services"
+}
+
+variable "instance_initiated_shutdown_behavior" {
+  description = "Shutdown behavior for the instance"
+  type        = string
+  default     = null
+}
+
+variable "instance_type" {
+  description = "The type of instance to start"
+  type        = string
+  default     = "t2.micro"
+}
+
+variable "ipv6_address_count" {
+  description = "A number of IPv6 addresses to associate with the primary network interface. Amazon EC2 chooses the IPv6 addresses from the range of your subnet."
+  type        = string
+  default     = null
+}
+
+variable "ipv6_addresses" {
+  description = "Specify one or more IPv6 addresses from the range of the subnet to associate with the primary network interface"
+  type        = list(string)
+  default     = []
+}
+
+variable "key_name" {
+  description = "The key name to use for the instance"
+  type        = string
+  default     = null
+}
+
+variable "launch_template" {
+  description = "Specifies a Launch Template to configure the instance, parameters configured on this resource will override the corresponding parameters in the Launch Template"
+  type        = map(any)
+  default     = {}
+}
+
+variable "maintenance_options" {
+  description = "Maintenance and recovery options for the instance"
+  type        = map(any)
+  default     = {}
+}
+
+variable "metadata_options" {
+  description = "Customize the metadata options of the instance"
+  type        = map(any)
+  default     = {}
+}
+
+variable "monitoring" {
+  description = "If true, the launched EC2 instance will have detailed monitoring enabled"
+  type        = bool
+  default     = false
+}
+
+variable "network_interface" {
+  description = "Customize network interfaces to be attached at instance boot time"
+  type        = list(any)
+  default     = []
+}
+
+variable "placement_group" {
+  description = "The Placement Group to start the instance in"
+  type        = string
+  default     = null
+}
+
+variable "placement_partition_number" {
+  description = "Number of the partition the instance is in, valid only if the aws_placement_group resource's strategy argument is set to 'partition'"
+  type        = number
+  default     = null
+}
+
+variable "private_dns_name_options" {
+  description = "Options for the instance hostname, the default values are inherited from the subnet"
+  type        = map(any)
+  default     = {}
+}
+
+variable "private_ips" {
+  description = "A list of private IP address to associate with the instance in a VPC. Should match the number of instances."
+  type        = list(string)
+  default     = []
+}
+
+variable "private_ip" {
+  description = "Private IP address to associate with the instance in a VPC"
+  type        = string
+  default     = null
+}
+
+variable "root_block_device" {
+  description = "Customize details about the root block device of the instance, see Block Devices below for details"
+  type        = list(any)
+  default     = []
+}
+
+variable "secondary_private_ips" {
+  description = "List of secondary private IPv4 addresses to assign to the instance's primary network interface (eth0) in a VPC"
+  type        = list(any)
+  default     = []
+}
+
+variable "security_groups" {
+  description = "List of security group names to associate with"
+  type        = list(any)
+  default     = []
+}
+
+variable "source_dest_check" {
+  description = "Controls if traffic is routed to the instance when the destination address does not match the instance. Used for NAT or VPNs."
+  type        = bool
+  default     = true
+}
+
+variable "subnet_id" {
+  description = "The VPC Subnet ID to launch in"
+  type        = string
+  default     = null
+}
+
+variable "use_num_suffix" {
+  description = "Always append numerical suffix to instance name, even if instance_count is 1"
+  type        = bool
+  default     = false
+}
+
+variable "name" {
+  description = "Name to be used on all resources as prefix"
+  type        = string
+  default     = "kloudjet_ec2"
+}
+
+variable "tags" {
+  description = "A mapping of string tags to assign to the EC2 resource"
+  type        = map(string)
+  default     = {}
+}
+
+variable "tenancy" {
+  description = "The tenancy of the instance (if the instance is running in a VPC). Available values: default, dedicated, host."
+  type        = string
+  default     = "default"
+}
+
+variable "user_data" {
+  description = "The user data to provide when launching the instance. Do not pass gzip-compressed data via this argument; see user_data_base64 instead."
+  type        = string
+  default     = null
+}
+
+variable "user_data_base64" {
+  description = "Can be used instead of user_data to pass base64-encoded binary data directly. Use this instead of user_data whenever the value is not a valid UTF-8 string. For example, gzip-encoded user data must be base64-encoded and passed via this argument to avoid corruption."
+  type        = string
+  default     = null
+}
+
+variable "user_data_replace_on_change" {
+  description = "When used in combination with user_data or user_data_base64 will trigger a destroy and recreate when set to true, defaults to false"
+  type        = bool
+  default     = null
+}
+
+variable "volume_tags" {
+  description = "A mapping of tags to assign to the devices created by the instance at launch time"
+  type        = map(any)
+  default     = {}
+}
+
+variable "volume_attachment_count" {
+  description = "Used in count for indexing and obtaining argument values in aws_volume_attachment and aws_ebs_volume resources"
+  type        = number
+  default     = 1
+}
+
+variable "aws_volume_attachment_device" {
+  description = "The device name to expose to the instance"
+  type        = string
+  default     = "xvdh"
+}
+
+variable "volume_force_detach" {
+  description = "Set to true if you want to force the volume to detach"
+  type        = bool
+  default     = false
+}
+
+variable "skip_destroy" {
+  description = "Set this to true if you do not wish to detach the volume from the instance to which it is attached at destroy time, and instead just remove the attachment from Terraform state"
+  type        = bool
+  default     = false
+}
+
+variable "stop_instance_before_detaching" {
+  description = "Set this to true to ensure that the target instance is stopped before trying to detach the volume"
+  type        = bool
+  default     = false
+}
+
+variable "ebs_encrypted" {
+  description = "If true, the disk will be encrypted"
+  type        = bool
+  default     = false
+}
+
+variable "final_snapshot" {
+  description = "If true, snapshot will be created before volume deletion, any tags on the volume will be migrated to the snapshot"
+  type        = bool
+  default     = false
+}
+
+variable "ebs_iops" {
+  description = "The amount of IOPS to provision for the disk"
+  type        = string
+  default     = null
+}
+
+variable "multi_attach_enabled" {
+  description = "Specifies whether to enable Amazon EBS Multi-Attach"
+  type        = bool
+  default     = false
+}
+
+variable "ebs_size" {
+  description = "The size of the drive in GiBs"
+  type        = number
+  default     = 2
+}
+
+variable "snapshot_id" {
+  description = "A snapshot to base the EBS volume off of"
+  type        = string
+  default     = null
+}
+
+variable "outpost_arn" {
+  description = "The Amazon Resource Name (ARN) of the Outpost"
+  type        = string
+  default     = null
+}
+
+variable "ebs_type" {
+  description = "The type of EBS volume"
+  type        = string
+  default     = null
+}
+
+variable "kms_key_id" {
+  description = "he ARN for the KMS encryption key"
+  type        = string
+  default     = null
+}
+
+variable "ebs_tags" {
+  description = "A map of tags to assign to the resource"
+  type        = map(any)
+  default     = {}
+}
+
+variable "throughput" {
+  description = "The throughput that the volume supports, in MiB/s"
+  type        = string
+  default     = null
+}
